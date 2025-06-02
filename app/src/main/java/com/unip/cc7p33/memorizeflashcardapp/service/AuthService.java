21522b903bb9744b5febb565e7745d155e05bff3
ferreiraffj <felipeffjcontato@gmail.com>
2025-06-02 19:43:06 -0300

    feat(auth): Salva dados do usuário no Firestore ao registrar
    Implementa a persistência de dados de novo usuário na coleção 'Users' utilizando o UID como documento ID.

--- app/src/main/java/com/unip/cc7p33/memorizeflashcardapp/service/AuthService.java
+++ app/src/main/java/com/unip/cc7p33/memorizeflashcardapp/service/AuthService.java
@@ -2,10 +2,15 @@ package com.unip.cc7p33.memorizeflashcardapp.service;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AuthService {
 
     private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
 
     public interface AuthCallback{
         void onSuccess(FirebaseUser user);
@@ -14,15 +19,27 @@ public class AuthService {
 
     public AuthService(){
         mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
     }
 
     public void registerUser(String email, String password, String nome, AuthCallback callback){
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(task -> {
                    if (task.isSuccessful()){
-                       FirebaseUser user = mAuth.getCurrentUser();
+                       FirebaseUser user = mAuth.getCurrentUser(); // atribui a variavel user o usuário autenticado
                        if (user != null){
-                           callback.onSuccess(user);
+                           Map<String, Object> userData = new HashMap<>(); // cria uma estrutura de dados do tipo chave-valor
+                           userData.put("nome", nome); // adiciona o nome do usuário ao Map
+                           userData.put("email", email); // adiciona o e-mail do usuário ao Map
+
+                           db.collection("users").document(user.getUid()) // cria ou utiliza a coleção "users" e define que o id do documento será o UID
+                                   .set(userData) // envia os dados do Map ao firestore
+                                   .addOnSuccessListener(aVoid ->{ // listener de sucesso
+                                       callback.onSuccess(user);
+                                   })
+                                   .addOnFailureListener(e ->{ // listener de exceção
+                                        callback.onFailure("Erro ao salvar os dados do usuário: " + e.getMessage());
+                                   });
                        } else {
                            callback.onFailure("Usuário não encontrado após o registro.");
                        }
